ae81c7cb42bc24d57899b439890a3f79c9f88c2f
Jai Bellare <jaibellare@gmail.com>
2025-02-20 20:20:23 +0530

    add Sponsor route

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Navbar from './Navbar.svelte';
+  let { children } = $props();
+</script>
+
+<Navbar />
+
+{@render children()}
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,4 @@
+<nav>
+  <a href="/">Home</a>
+  <a href="/sponsor">Sponsor</a>
+</nav>
