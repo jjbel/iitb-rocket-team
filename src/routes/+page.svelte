d253bff225378b8d2c5e5c31cfad900f53247d90
Jai Bellare <jaibellare@gmail.com>
2025-02-20 20:15:51 +0530

    formatting, css: remove whitespace

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div id="container">
+  <h1>Welcome to SvelteKit</h1>
+  <p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+  </p>
+</div>
+
+<style>
+  /* remove top space,  */
+  :global(*) {
+    margin: 0;
+    padding: 0;
+
+    color: white;
+    background-color: rgb(18, 18, 23);
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  #container {
+  }
+</style>
