2d87f4548eb369f1b7e1d18c012429d7bce72e93
Jai Bellare <jaibellare@gmail.com>
2025-02-23 23:45:37 +0530

    split into separate components, add images

---
+++ src/routes/home/Intro.svelte
@@ -0,0 +1,83 @@
+<script>
+  import ChevronsRight from 'lucide-svelte/icons/chevrons-right';
+</script>
+
+<div id="intro">
+  <div id="title">
+    <h2>Reaching New</h2>
+    <h2>Frontiers in</h2>
+    <h1 id="h1-glow">High Powered</h1>
+    <h1>Rocketry</h1>
+  </div>
+
+  <a href="/contact" id="contact">
+    Contact Us <ChevronsRight id="chevrons" color="#000000" />
+  </a>
+</div>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Yellowtail&display=swap');
+
+  #intro {
+    padding: 16vh 10vw 0 10vw;
+
+    background-image:
+      linear-gradient(to right, rgb(0, 0, 0) 50%, rgba(0, 0, 0, 0) 70%),
+      url('/agastya-5817.jpg');
+
+    background-size: cover;
+    background-position:
+      0 0,
+      30vw 0vh;
+
+    height: calc(100vh - var(--navbar-height));
+  }
+
+  #title {
+    background: linear-gradient(135deg, #ffffff, #b1b4fc);
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  #intro h2 {
+    font-size: 3rem;
+  }
+
+  #intro h1 {
+    font-size: 5rem;
+  }
+
+  #h1-glow {
+    text-shadow: rgba(232, 242, 255, 0.76) 0 0 0.7rem;
+  }
+
+  #contact {
+    display: flex;
+    width: fit-content;
+    align-items: center;
+    gap: 0.3rem;
+
+    background-color: white;
+    color: black;
+    border: none;
+    font-weight: 600;
+
+    font-size: 1.6rem;
+    border-radius: 0.4rem;
+    padding: 1rem;
+    margin-top: 8vh;
+  }
+
+  @media (max-aspect-ratio: 1) {
+    #intro {
+      background-image:
+        linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+        url('/agastya-5817.jpg');
+
+      background-size: cover;
+      background-position:
+        0 0,
+        50% 0vh;
+    }
+  }
+</style>
---
+++ src/routes/home/Tech.svelte
@@ -0,0 +1,115 @@
+<script>
+  import flyte from '$lib/flyte.jpg';
+  import telemetry from '$lib/telemetry.jpg';
+</script>
+
+<div id="tech">
+  <h2>Feat. the <span id="hottest">Hottest</span> Tech</h2>
+  <ul>
+    <li>
+      <h3>FLYTE</h3>
+
+      <p>
+        Our latest flight computer.
+
+        <a href="https://github.com/IITB-Rocket-Team-Avionics/FLYTE"
+          >Check it out on Github</a
+        >
+      </p>
+
+      <img src={flyte} alt="FLYTE computers" />
+      <span>2 years of PCB redesign.</span>
+    </li>
+
+    <li>
+      <h3>Ground Station</h3>
+
+      <p>
+        Stream and view telemetry in realtime.
+
+        <a href="https://github.com/jjbel/pi-pico-telemetry"
+          >Check it out on Github</a
+        >
+      </p>
+
+      <img src={telemetry} alt="" />
+      <span>Data being streamed, denoised, and graphed in realtime</span>
+    </li>
+
+    <li>
+      <h3>Payload Tracking</h3>
+
+      <p>
+        Advanced 4-axis gyro tracking and payload detection using Neural
+        Networks.
+
+        <!-- <a href="https://github.com/jjbel/pi-pico-telemetry"
+          >Check it out on Github</a
+          TODO
+        > -->
+      </p>
+
+      <!-- <img src={telemetry} alt="" /> -->
+      <!-- <span>Data being streamed, denoised, and graphed in realtime</span> -->
+    </li>
+  </ul>
+</div>
+
+<style>
+  #tech {
+    padding: 2vh 10% 2vh 10%;
+  }
+
+  h2 {
+    font-size: 3.2rem;
+    vertical-align: middle;
+  }
+
+  #hottest {
+    font-family: 'Yellowtail', serif;
+    font-size: 4.5rem;
+    padding-right: 1.1rem;
+
+    background: linear-gradient(to right, #ff2066, #ff5618);
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  ul {
+    font-size: 1.4rem;
+    margin-top: 0.2rem;
+
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 1rem;
+  }
+
+  ul p {
+    font-size: 1rem;
+    margin-top: 1rem;
+  }
+
+  li {
+    background-color: rgba(104, 104, 104, 0.13);
+    padding: 0.8rem;
+    border-radius: 0.6rem;
+    width: 22rem;
+    height: fit-content;
+  }
+
+  img {
+    display: block;
+    width: 100%;
+    margin-top: 1rem;
+  }
+
+  ul span {
+    color: rgb(220, 220, 220);
+    font-size: 1rem;
+  }
+
+  ul a {
+    text-decoration: underline;
+  }
+</style>
