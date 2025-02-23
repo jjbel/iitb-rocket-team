e9c4b36a94a70145d0a8c0572890dfb2eed266fe
Jai Bellare <jaibellare@gmail.com>
2025-02-23 18:23:50 +0530

    Navbar: add links, logo, css: flex

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,9 +1,48 @@
 <script>
   // https://www.okupter.com/blog/deploy-sveltekit-website-to-github-pages
   import { base } from '$app/paths';
+
+  import logo from '$lib/rtlogo1.png'
+
+  const links = [
+    ['/', 'Home'],
+    ['/sponsor', 'Sponsor'],
+    ['/sponsor', 'Teams'],
+    ['/sponsor', 'Contact']
+  ];
 </script>
 
 <nav>
-  <a href="{base}/">Home</a>
-  <a href="{base}/sponsor">Sponsor</a>
+  <img src={logo} alt="" />
+
+  <div class="links">
+    {#each links as link}
+      <a href="{base}/{link[0]}">{link[1]}</a>
+    {/each}
+  </div>
 </nav>
+
+<style>
+  nav {
+    display: flex;
+    /* background-color: rgb(98, 98, 98); */
+
+    color: rgb(232, 232, 232);
+    padding: 0 4rem;
+    height: var(--navbar-height);
+    justify-content: center;
+  }
+
+  img {
+    height: 100%;
+  }
+
+  .links {
+    display: flex;
+    padding: 0.9rem 4rem;
+    font-size: 1.4rem;
+    gap: 3rem;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
