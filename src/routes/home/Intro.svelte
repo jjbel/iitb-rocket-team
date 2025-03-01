2920969b55bb0afe3da5f4d6b9a8cc54e7e53034
Jai Bellare <jaibellare@gmail.com>
2025-03-01 20:14:01 +0530

    Intro: add agastya caption

--- src/routes/home/Intro.svelte
+++ src/routes/home/Intro.svelte
@@ -1,5 +1,7 @@
 <script>
   import ChevronsRight from 'lucide-svelte/icons/chevrons-right';
+  import ChevronsUp from 'lucide-svelte/icons/chevrons-up';
+
   import { base } from '$app/paths';
 </script>
 
@@ -13,13 +15,25 @@
   </div>
 
   <a href="{base}/contact" id="contact">
-    Contact Us <ChevronsRight id="chevrons" color="var(--bg)" />
+    Contact Us <ChevronsRight id="contact-chevrons" color="var(--bg)" />
   </a>
+
+  <div id="caption">
+    <div id="caption_chevrons_mobile">
+      <ChevronsUp size="2rem" color="var(--fg)" />
+    </div>
+
+    <div>Agastya leaving a trail of smoke and fire in its wake.</div>
+
+    <div id="caption_chevrons_desktop">
+      <ChevronsRight size="2rem" color="var(--fg)" />
+    </div>
+  </div>
 </div>
 
 <style>
   #intro {
-    padding: 16vh 10vw 0 10vw;
+    padding: 10vh 10vw 0 10vw;
 
     background-image:
       linear-gradient(to right, var(--bg) 45%, transparent 80%),
@@ -33,6 +47,8 @@
     height: calc(100vh - var(--navbar-height));
 
     border-bottom: 1.5px solid var(--fg-gray);
+
+    position: relative;
   }
 
   /* on mobile, the bg image is dark, so use the light theme styling only on desktop */
@@ -84,16 +100,58 @@
     margin-top: 8vh;
   }
 
+  #caption {
+    position: absolute;
+    left: 45%;
+    bottom: 1rem;
+    transform: translateX(-100%);
+
+    width: 15rem;
+
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+    background-color: rgba(155, 155, 155, 0.13);
+    padding: 0.8rem;
+    border-radius: 0.6rem;
+    border: 0.1rem solid var(--fg-gray);
+  }
+
+  #caption_chevrons_mobile {
+    display: none;
+  }
+
+  #caption_chevrons_desktop {
+    display: block;
+  }
+
   @media (max-aspect-ratio: 1) {
     #intro {
       background-image:
         linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-        url('/agastya-5817.jpg');
+        var(--bg-image-url);
 
       background-size: cover;
       background-position:
         0 0,
         50% 0vh;
+    }
+
+    #caption {
+      width: 20rem;
+      color: rgb(212, 212, 212);
+      left: 10%;
+      bottom: 5rem;
+      transform: none;
+    }
+
+    #caption_chevrons_mobile {
+      display: block;
+    }
+
+    #caption_chevrons_desktop {
+      display: none;
     }
   }
 </style>
